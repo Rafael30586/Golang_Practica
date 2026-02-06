cfd390a7c7ea2cf81ff2e370e6fe75ad26e55ca0
Rafael305 <fraq86@gmail.com>
2026-02-06 18:12:30 -0300

    bucle for

--- bucles/bucles.go
+++ bucles/bucles.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+
 	for i := 0; i < 5; i++ {
 		fmt.Println("I es igual a:", i)
 	}
@@ -18,4 +19,13 @@ func main() {
 		}
 		fmt.Println("i es igual a :", i)
 	}
+
+	fmt.Println("-----------------------------------")
+
+	var numeros = [...]int16{34, 90, 120, 82, 50, 12, 94}
+
+	for indice, valor := range numeros {
+		fmt.Println("El índice es", indice, "y el valor es", valor)
+	}
+
 }
