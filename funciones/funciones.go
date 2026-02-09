5b58987fe8a39e6bc9a12238d0aa222443e0fcb5
Rafael305 <fraq86@gmail.com>
2026-02-09 18:26:33 -0300

    funciones variadicas

--- funciones/funciones.go
+++ funciones/funciones.go
@@ -9,6 +9,7 @@ func main() {
 	Saludar()
 	Sumar(16, 9)
 	fmt.Println(Presentarse("Gustavo", 27))
+	NombrarPersonajes("Master Chief", "Kratos", "Kirby", "Sonic", "Link")
 }
 
 func Saludar() {
@@ -22,4 +23,13 @@ func Sumar(a int, b int) {
 func Presentarse(nombre string, edad int) string {
 	presentacion := "Me llamo " + nombre + " y tengo " + strconv.Itoa(edad) + " años de edad."
 	return presentacion
+}
+
+func NombrarPersonajes(personajes ...string) {
+	longitud := len(personajes)
+
+	for i := 0; i < longitud; i++ {
+		fmt.Println(personajes[i])
+	}
+
 }
