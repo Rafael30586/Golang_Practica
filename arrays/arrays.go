289515a80e4afdb6503c9d787b04ec5b17d98a39
Rafael305 <fraq86@gmail.com>
2026-02-13 18:04:17 -0300

    prueba con punteros y arrays

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -19,4 +19,18 @@ func main() {
 	edades = append(edades, 23, 45, 10) // De esta manera se adicionan elementos a un slice
 	fmt.Println("Ahora el slice es:", edades)
 
+	var frutas2 = frutas
+
+	fmt.Println("frutas2 es:", frutas2)
+	frutas[0] = "limón"
+	fmt.Println("Ahora frutas2 es:", frutas2) // frutas2 sigue inalterado, al parece el lugar en memoria es distinto a frutas
+
+	var frutas3 = &frutas // frutas3 es un puntero que apunta a frutas
+
+	fmt.Println("frutas3 es: ", frutas3)
+	frutas[3] = "damasco"
+	fmt.Println("Ahora frutas3 es igual a:", frutas3)
+	frutas3[1] = "frambuesa" // cuando cambio a frutas3 también cambias frutas
+	fmt.Println("Ahota frutas es ", frutas)
+
 }
